e094f8e757a4d890767ee863ae9e53736375ce30
Miguel Petherson <147556184+pethersonzada@users.noreply.github.com>
2024-11-19 12:12:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// Importando a biblioteca Express para criar a aplicação web
 import express from "express";
 
+// Criando uma nova aplicação Express
 const app = express();
+
+// Iniciando o servidor na porta 3000 e mostrando uma mensagem no console
 app.listen(3000, () => {
-    console.log("Escutando na porta 3000...");
+    console.log("Servidor iniciado na porta 3000!");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Hello World!");
-}); 
+// Definindo uma rota para a raiz da aplicação (/)
+app.get("/", (req, res) => {
+    // Enviando a mensagem "Hello, World!" como resposta
+    res.send("Hello, World!");
+});
